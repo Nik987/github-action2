bf6da183f08b487840a8c03a25abc171a396e538
NIKHIL BALU NANEKAR <63490589+Nik987@users.noreply.github.com>
2023-11-03 00:06:18 +0530

    Merge remote changes into local main

--- demo-ctl-hrms-infra-main/backend.tf
+++ demo-ctl-hrms-infra-main/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "ctl-tf-state-lock-demo2"
-    key            = "s32"
+    key            = "s324"
     region         = "us-east-1"
     dynamodb_table = "terraform"
   }
